352da19e3afc518c629b6dd2a8132861109969a2
AraLawrence <ara.e.lawrence@gmail.com>
2015-12-08 20:35:32 -0800

    search

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ var Remark = require('./models/remark');
 var Affirm = require('./models/affirm');
 mongoose.connect('mongodb://localhost/holiday-party');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/api/remark', require('./controllers/remark'));
 app.use('/api/affirm', require('./controllers/affirm'));
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'app/index.html'));
+});
 
 app.listen(3000);
